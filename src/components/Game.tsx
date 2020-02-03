3d0a00855fc6c330ecda4da471e4cb79b3c674b3
Steve Reid <steve@appby.design>
2020-02-03 16:44:52 +0000

    refactor game component from class to fuctional, and hooks, and swap immer for use-immer

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
-import produce from 'immer';
+import { useImmer } from 'use-immer';
 import { Question } from './Question';
 import { loadQuestions } from '../utils/questionHelper';
 import { HUD } from './HUD';
@@ -32,95 +32,88 @@ interface GameState {
   done: boolean;
 }
 
-export default class Game extends Component<GameProps, GameState> {
-  constructor(props: GameProps) {
-    super(props);
+const Game: React.FC<GameProps> = ({ history }: GameProps) => {
+  const [questions, setQuestions] = useState<QuestionType[] | null>([]);
+  const [currentQuestion, setCurrentQuestion] = useState<QuestionType | null>(
+    null
+  );
+  const [loading, setLoading] = useState(true);
+  const [done, setDone] = useState(false);
+  const [score, updateScore] = useImmer({ value: 0 });
+  const [questionNumber, updateQuestionNumber] = useImmer({ value: 0 });
 
-    this.state = {
-      questions: null,
-      currentQuestion: null,
-      loading: true,
-      score: 0,
-      questionNumber: 0,
-      done: false,
-    };
-  }
+  useEffect(() => {
+    loadQuestions()
+      .then(questions => setQuestions(questions))
+      .catch(err => console.error(err));
+  }, []);
 
-  async componentDidMount(): Promise<void> {
-    try {
-      const questions = await loadQuestions();
-      this.setState(
-        {
-          questions,
-        },
-        () => {
-          this.changeQuestion();
-        }
-      );
-    } catch (err) {
-      console.error(err);
+  const changeQuestion = useCallback(
+    (bonus = 0): void => {
+      if (questions && questions.length === 0) {
+        setDone(true);
+        return updateScore(draft => {
+          draft.value += bonus;
+        });
+      }
+      if (questions) {
+        const randomQuestionIndex = Math.floor(
+          Math.random() * questions.length
+        );
+        // randomly get a question
+        const currentQuestion = questions[randomQuestionIndex];
+        // copy questions array
+        const remainingQuestions = [...questions];
+        // remove currentQuestion from remaining questions
+        remainingQuestions.splice(randomQuestionIndex, 1);
+        // set remaining questions to state
+
+        setQuestions(remainingQuestions);
+        setCurrentQuestion(currentQuestion);
+        setLoading(false);
+        updateScore(draft => {
+          draft.value += bonus;
+        });
+        updateQuestionNumber(draft => {
+          draft.value += 1;
+        });
+      }
+    },
+    [
+      questions,
+      updateScore,
+      setQuestions,
+      setLoading,
+      setCurrentQuestion,
+      updateQuestionNumber,
+    ]
+  );
+
+  useEffect(() => {
+    if (questions && questions.length && !currentQuestion) {
+      changeQuestion();
     }
-  }
+  }, [changeQuestion, questions, currentQuestion]);
 
-  changeQuestion = (bonus = 0): void => {
-    if (this.state.questions && this.state.questions.length === 0) {
-      return this.setState(
-        produce(draft => {
-          draft.score += bonus;
-          draft.done = true;
-        })
-      );
-    }
-    if (this.state.questions) {
-      const randomQuestionIndex = Math.floor(
-        Math.random() * this.state.questions.length
-      );
-      // randomly get a question
-      const currentQuestion = this.state.questions[randomQuestionIndex];
-      // copy questions array
-      const remainingQuestions = [...this.state.questions];
-      // remove currentQuestion from remaining questions
-      remainingQuestions.splice(randomQuestionIndex, 1);
-      // set remaining questions to state
-
-      this.setState(
-        produce(draft => {
-          draft.questions = remainingQuestions;
-          draft.currentQuestion = currentQuestion;
-          draft.loading = false;
-          draft.score = draft.score += bonus;
-          draft.questionNumber += 1;
-        })
-      );
-    }
+  const scoreSaved = (): void => {
+    history.push('/');
   };
 
-  scoreSaved = (): void => {
-    this.props.history.push('/');
-  };
+  return (
+    <>
+      {loading && !done && <div id="loader" />}
+      {!done && !loading && currentQuestion && (
+        <>
+          <HUD score={score.value} questionNumber={questionNumber.value} />
+          <Question
+            changeQuestion={changeQuestion}
+            question={currentQuestion}
+          />
+        </>
+      )}
+      {done && <SaveScoreForm score={score.value} scoreSaved={scoreSaved} />}
+    </>
+  );
+};
 
-  render(): JSX.Element {
-    const {
-      loading,
-      done,
-      score,
-      currentQuestion,
-      questionNumber,
-    } = this.state;
-    return (
-      <>
-        {loading && !done && <div id="loader" />}
-        {!done && !loading && currentQuestion && (
-          <>
-            <HUD score={score} questionNumber={questionNumber} />
-            <Question
-              changeQuestion={this.changeQuestion}
-              question={currentQuestion}
-            />
-          </>
-        )}
-        {done && <SaveScoreForm score={score} scoreSaved={this.scoreSaved} />}
-      </>
-    );
-  }
-}
+export default Game;
